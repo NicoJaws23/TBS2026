4793a94bde30e2b52e8fed5d30038cca1f7ca975
NicoJaws23 <nico.jaworski@utexas.edu>
2026-04-08 10:55:01 -0500

    April8_2026_1054

---
+++ geoSpat.R
@@ -0,0 +1,8 @@
+#GeoSpatial Work
+
+library(tidyverse)
+library(ggplot2)
+
+files <- list.files(path = "C:\\Users\\Jawor\\Desktop\\Research\\LagoD_WD\\EXTRACT_15_WINDOW_120",
+                    pattern = "*.csv",
+                    full.names = TRUE)
