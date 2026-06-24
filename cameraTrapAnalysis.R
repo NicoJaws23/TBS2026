d898fd75cc703dd2e8c907b9a6ece7ce0977230d
NicoJaws23 <nico.jaworski@utexas.edu>
2026-06-24 10:03:24 -0500

    2026-06-24, 10:03

--- cameraTrapAnalysis.R
+++ cameraTrapAnalysis.R
@@ -22,6 +22,12 @@ df <- df|>
 
 df_full <- left_join(df, wp, by = "MonitorID")
 
+trails <- st_read("https://raw.githubusercontent.com/NicoJaws23/creative-data-visualization/refs/heads/main/TBS_Trails.geojson") |>
+  st_transform(4326)
+river <- st_read("https://raw.githubusercontent.com/NicoJaws23/creative-data-visualization/refs/heads/main/rio_tiputini.geojson") |>
+  st_transform(4326)
+
+
 #Clean ScientificName
 
 df$ScientificName <- trimws(df$ScientificName)  # removes leading/trailing spaces (fixes Leptotilla)
@@ -60,7 +66,7 @@ ggplot(species_summary, mapping = aes(x = ScientificName, y = TotalDetections)) 
 
 #Heatmaps with leaflet#
 
-expand_for_heatmap <- function(data) {
+expand_for_heatmap_1 <- function(data) {
   data[rep(seq_len(nrow(data)), times = data$TotalDetections), ]
 }
 
@@ -71,22 +77,38 @@ color_palette <- colorFactor(
   domain  = species_list
 )
 
-map <- leaflet() %>%
+map_1 <- leaflet() %>%
   addProviderTiles(providers$Esri.WorldImagery, group = "Satellite") %>%
   addProviderTiles(providers$OpenStreetMap,     group = "Street Map") %>%
+  
+  # ── Static layers ────────────────────────────────────────────────────────────
+  addPolylines(
+    data   = trails,
+    color  = "black",
+    weight = 2,
+    group  = "Trails",
+    label  = "Trails"
+  ) %>%
+  addPolylines(
+    data   = river,
+    color  = "blue",
+    weight = 2,
+    group  = "River",
+    label  = "River"
+  ) %>%
   addLayersControl(
     baseGroups    = c("Satellite", "Street Map"),
-    overlayGroups = species_list,
+    overlayGroups = c("Trails", "River", species_list),
     options       = layersControlOptions(collapsed = TRUE)
   )
 
 for (sp in species_list) {
   
   sp_data     <- species_summary %>% filter(ScientificName == sp)
-  sp_expanded <- expand_for_heatmap(sp_data)
+  sp_expanded <- expand_for_heatmap_1(sp_data)
   sp_color    <- color_palette(sp)   # unique color for this species
   
-  map <- map %>%
+  map_1 <- map_1 %>%
     addHeatmap(
       data   = sp_expanded,
       lat    = ~lat,
@@ -114,9 +136,9 @@ for (sp in species_list) {
     )
 }
 
-map <- map %>% hideGroup(species_list)
+map_1 <- map_1 %>% hideGroup(species_list)
 
-map
+map_1
 
 #Sitings of number of individuals of species by day by monitor####
 df_uniqueSite <- df_full |>
@@ -132,3 +154,79 @@ uniqueSiteSummary <- left_join(uniqueSiteSummary, wp, by = "MonitorID")
 ggplot(uniqueSiteSummary, mapping = aes(x = ScientificName, y = TotalDetectionDays)) +
   geom_col() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+species_list_2 <- sort(unique(uniqueSiteSummary$ScientificName))
+
+color_palette <- colorFactor(
+  palette = "Set3",       # try also "Set1", "Set2", "Dark2", or "Paired"
+  domain  = species_list
+)
+
+expand_for_heatmap_2 <- function(data) {
+  data[rep(seq_len(nrow(data)), times = data$TotalDetectionDays), ]
+}
+
+map_2 <- leaflet() %>%
+  addProviderTiles(providers$Esri.WorldImagery, group = "Satellite") %>%
+  addProviderTiles(providers$OpenStreetMap,     group = "Street Map") %>%
+  
+  # ── Static layers ────────────────────────────────────────────────────────────
+  addPolylines(
+    data   = trails,
+    color  = "black",
+    weight = 2,
+    group  = "Trails",
+    label  = "Trails"
+  ) %>%
+  addPolylines(
+    data   = river,
+    color  = "blue",
+    weight = 2,
+    group  = "River",
+    label  = "River"
+  ) %>%
+  addLayersControl(
+    baseGroups    = c("Satellite", "Street Map"),
+    overlayGroups = c("Trails", "River", species_list),
+    options       = layersControlOptions(collapsed = TRUE)
+  )
+
+for (sp in species_list) {
+  
+  sp_data     <- uniqueSiteSummary %>% filter(ScientificName == sp)
+  sp_expanded <- expand_for_heatmap_2(sp_data)
+  sp_color    <- color_palette(sp)   # unique color for this species
+  
+  map_2 <- map_2 %>%
+    addHeatmap(
+      data   = sp_expanded,
+      lat    = ~lat,
+      lng    = ~lon,
+      blur   = 30,
+      max    = 1,
+      radius = 25,
+      group  = sp
+    ) %>%
+    addCircleMarkers(
+      data        = sp_data,
+      lat         = ~lat,
+      lng         = ~lon,
+      radius      = ~scales::rescale(TotalDetectionDays, to = c(3, 12)),
+      color       = "white",
+      fillColor   = sp_color,
+      fillOpacity = 0.8,
+      weight      = 1,
+      popup       = ~paste0(
+        "<b>", ScientificName, "</b><br>",
+        "Monitor: ", MonitorID, "<br>",
+        "Detection Days: ", TotalDetectionDays
+      ),
+      group = sp
+    )
+}
+
+map_2 <- map_2 %>% hideGroup(species_list)
+
+map_2
+
+map_1
